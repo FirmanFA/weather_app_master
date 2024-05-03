2b789e8a18faac46d5d07cde311b50e5cedddc74
firmanfa <fafirmansyah20@gmail.com>
2024-05-03 22:41:36 +0700

    feat: dynamically displaying selected city

--- lib/presentation/main/view/page/main_page.dart
+++ lib/presentation/main/view/page/main_page.dart
@@ -17,196 +17,205 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder(
-      init: MainController(),
-      builder: (controller) {
-        return Scaffold(
-          backgroundColor: primaryColor,
-          body: SafeArea(
-            child: Column(
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                24.verticalSpace,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+        init: MainController(),
+        builder: (controller) {
+          return Obx(() {
+            return Scaffold(
+              backgroundColor: primaryColor,
+              body: SafeArea(
+                child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "San Francisco",
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w600),
-                    ),
-                    4.horizontalSpace,
-                    Icon(
-                      Iconsax.arrow_down_1,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-                LottieBuilder.asset(
-                  "assets/lottie/weather/night_bright.json",
-                  width: Get.width,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Table(
-                    children: [
-                      TableRow(children: [
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.bottom,
-                          child: FittedBox(
-                            fit: BoxFit.scaleDown,
-                            child: Text(
-                              "54\u00B0",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w900, fontSize: 200.sp),
-                            ),
-                          ),
+                    24.verticalSpace,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          controller.selectedCityString.value,
+                          textAlign: TextAlign.center,
+                          style:
+                          TextStyle(
+                              fontSize: 20.sp, fontWeight: FontWeight.w600),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            SizedBox(
-                              height: 60,
+                        4.horizontalSpace,
+                        Icon(
+                          Iconsax.arrow_down_1,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                    LottieBuilder.asset(
+                      "assets/lottie/weather/night_bright.json",
+                      width: Get.width,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Table(
+                        children: [
+                          TableRow(children: [
+                            TableCell(
+                              verticalAlignment: TableCellVerticalAlignment
+                                  .bottom,
+                              child: FittedBox(
+                                fit: BoxFit.scaleDown,
+                                child: Text(
+                                  "54\u00B0",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 200.sp),
+                                ),
+                              ),
                             ),
-                            SizedBox(
-                              height: 60,
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                SizedBox(
+                                  height: 60,
+                                ),
+                                SizedBox(
+                                  height: 60,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment
+                                        .start,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "Thunderstorm",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          letterSpacing: 0.7,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Feels like: 60\u00B0",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          letterSpacing: 0.7,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 30,
+                            child: Container(
+                              height: 128,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.withOpacity(0.3),
+                                borderRadius: BorderRadius.circular(16.r),
+                              ),
+                              padding: EdgeInsets.all(12),
                               child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.end,
                                 children: [
-                                  Text(
-                                    "Thunderstorm",
+                                  AutoSizeText(
+                                    "TODAY",
                                     style: TextStyle(
+                                      fontSize: 10.sp,
                                       fontWeight: FontWeight.w600,
-                                      letterSpacing: 0.7,
                                     ),
                                   ),
-                                  Text(
-                                    "Feels like: 60\u00B0",
+                                  LottieBuilder.asset(
+                                    "assets/lottie/weather/night_bright.json",
+                                    width: Get.width,
+                                  ),
+                                  AutoSizeText(
+                                    "50\u00B0/60\u00B0",
                                     style: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: 0.7,
+                                      fontSize: 14.sp,
+                                      fontWeight: FontWeight.w700,
                                     ),
                                   ),
                                 ],
                               ),
                             ),
-                          ],
-                        ),
-                      ]),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 30,
-                        child: Container(
-                          height: 128,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.withOpacity(0.3),
-                            borderRadius: BorderRadius.circular(16.r),
                           ),
-                          padding: EdgeInsets.all(12),
-                          child: Column(
-                            children: [
-                              AutoSizeText(
-                                "TODAY",
-                                style: TextStyle(
-                                  fontSize: 10.sp,
-                                  fontWeight: FontWeight.w600,
-                                ),
+                          12.horizontalSpace,
+                          Expanded(
+                            flex: 75,
+                            child: Container(
+                              height: 128,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.withOpacity(0.3),
+                                borderRadius: BorderRadius.circular(16.r),
                               ),
-                              LottieBuilder.asset(
-                                "assets/lottie/weather/night_bright.json",
-                                width: Get.width,
-                              ),
-                              AutoSizeText(
-                                "50\u00B0/60\u00B0",
-                                style: TextStyle(
-                                  fontSize: 14.sp,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      12.horizontalSpace,
-                      Expanded(
-                        flex: 75,
-                        child: Container(
-                          height: 128,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.withOpacity(0.3),
-                            borderRadius: BorderRadius.circular(16.r),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              12.verticalSpace,
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 12),
-                                child: AutoSizeText(
-                                  "5-DAY FORECAST",
-                                  style: TextStyle(
-                                    fontSize: 10.sp,
-                                    fontWeight: FontWeight.w600,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  12.verticalSpace,
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 12),
+                                    child: AutoSizeText(
+                                      "5-DAY FORECAST",
+                                      style: TextStyle(
+                                        fontSize: 10.sp,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
                                   ),
-                                ),
-                              ),
-                              Expanded(
-                                child: ListView.separated(
-                                  scrollDirection: Axis.horizontal,
-                                  // shrinkWrap: true,
-                                  padding: EdgeInsets.all(12),
-                                  itemBuilder: (context, index) {
-                                    return Container(
-                                      width: 60,
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                            color: Colors.grey.withOpacity(0.3)),
-                                        color: Colors.grey.withOpacity(0.1),
-                                        borderRadius: BorderRadius.circular(12.r),
-                                      ),
-                                      padding: EdgeInsets.all(8),
-                                      child: Column(
-                                        children: [
-                                          LottieBuilder.asset(
-                                            "assets/lottie/weather/night_bright.json",
+                                  Expanded(
+                                    child: ListView.separated(
+                                      scrollDirection: Axis.horizontal,
+                                      // shrinkWrap: true,
+                                      padding: EdgeInsets.all(12),
+                                      itemBuilder: (context, index) {
+                                        return Container(
+                                          width: 60,
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Colors.grey.withOpacity(
+                                                    0.3)),
+                                            color: Colors.grey.withOpacity(0.1),
+                                            borderRadius: BorderRadius.circular(
+                                                12.r),
                                           ),
-                                          Text(
-                                            "50\u00B0/60\u00B0",
-                                            style: TextStyle(
-                                              fontSize: 9.sp,
-                                              fontWeight: FontWeight.w700,
-                                            ),
+                                          padding: EdgeInsets.all(8),
+                                          child: Column(
+                                            children: [
+                                              LottieBuilder.asset(
+                                                "assets/lottie/weather/night_bright.json",
+                                              ),
+                                              Text(
+                                                "50\u00B0/60\u00B0",
+                                                style: TextStyle(
+                                                  fontSize: 9.sp,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              ),
+                                            ],
                                           ),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                  separatorBuilder: (context, index) =>
+                                        );
+                                      },
+                                      separatorBuilder: (context, index) =>
                                       10.horizontalSpace,
-                                  itemCount: 5,
-                                ),
-                              )
-                            ],
+                                      itemCount: 5,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    Spacer(),
+                    Text("Made with love, firman <3")
+                  ],
                 ),
-                Spacer(),
-                Text("Made with love, firman <3")
-              ],
-            ),
-          ),
-        );
-      }
+              ),
+            );
+          });
+        }
     );
   }
 }
