2faf2a3a8591c22be6d7b5b34972b731f0d16909
firmanfa <fafirmansyah20@gmail.com>
2024-05-03 11:35:17 +0700

    weather_repository: add get forecast method

--- lib/core/repository/weather_repository.dart
+++ lib/core/repository/weather_repository.dart
@@ -3,6 +3,7 @@ import 'package:weather_app_master/core/remote/custom_exception.dart';
 import 'package:weather_app_master/core/remote/dio_service.dart';
 import 'package:weather_app_master/core/remote/endpoint/api_endpoint.dart';
 import 'package:weather_app_master/core/remote/endpoint/enum_endpoint.dart';
+import 'package:weather_app_master/presentation/main/model/forecast/get_forecast_response.dart';
 import 'package:weather_app_master/presentation/main/model/weather/get_weather_response.dart';
 import 'package:weather_app_master/utils/typedefs.dart';
 
@@ -19,6 +20,20 @@ class WeatherRepository {
         queryParams: queryParams,
         requiresAuthToken: true,
         converter: (response) => GetWeatherResponse.fromJson(response),
+      );
+    } on CustomException {
+      rethrow;
+    }
+  }
+
+  Future<GetForecastResponse> get5DayForecast(
+      {required JSON queryParams}) async {
+    try {
+      return _apiService.getDocumentData<GetForecastResponse>(
+        endpoint: ApiEndpoint.weather(WeatherEndpoint.get5DayForecast),
+        queryParams: queryParams,
+        requiresAuthToken: true,
+        converter: (response) => GetForecastResponse.fromJson(response),
       );
     } on CustomException {
       rethrow;
