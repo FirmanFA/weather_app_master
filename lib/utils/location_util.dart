a5ff32c7d74c7eb3ebf5c35024e6ba62d38ab4d0
firmanfa <fafirmansyah20@gmail.com>
2024-05-03 21:46:21 +0700

    add location util

---
+++ lib/utils/location_util.dart
@@ -0,0 +1,53 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
+class LocationUtil {
+  static Future<Position?> determinePosition() async {
+    LocationPermission permission;
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return null;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return null;
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    return position;
+  }
+
+  static Future<String> getCurrentCityFromLongLat(
+      {required double long, required double lat}) async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      lat,
+      long,
+    );
+    if (placemarks.isNotEmpty) {
+      String city = placemarks[0].subAdministrativeArea ?? "";
+      return city;
+    }
+
+    return "";
+  }
+}
+
+extension PositionUtil on Position {
+  Future<String> get getCityName async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      latitude,
+      longitude,
+    );
+    if (placemarks.isNotEmpty) {
+      String city = placemarks[0].subAdministrativeArea ?? "";
+      return city;
+    }
+
+    return "";
+  }
+}
