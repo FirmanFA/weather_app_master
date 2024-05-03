8e2161abdae46eab66639d3805cae3067d6bb666
firmanfa <fafirmansyah20@gmail.com>
2024-05-03 22:36:16 +0700

    feat: create get user position method

--- lib/presentation/main/controller/main_controller.dart
+++ lib/presentation/main/controller/main_controller.dart
@@ -1,6 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:get/get.dart';
+import 'package:weather_app_master/utils/location_util.dart';
 
 class MainController extends GetxController {
+  var selectedCityString = "".obs;
+  Rxn<Location> selectedLocation = Rxn();
 
+  @override
+  void onInit() {
+    getUserPosition();
 
+    super.onInit();
+  }
+
+  getUserPosition() {
+    LocationUtil.determinePosition().then((value) async {
+      if (value != null) {
+        setSelectedCity(await value.getCityName);
+        setSelectedLocation(value.toLocation);
+      } else {}
+    });
+  }
+
+  setSelectedCity(String value) {
+    selectedCityString.value = value;
+    debugPrint("selected city: ${selectedCityString.value}");
+    update();
+  }
+
+  setSelectedLocation(Location value) {
+    selectedLocation.value = value;
+    debugPrint("selected location: ${selectedLocation.value?.toString()}");
+    update();
+  }
 }
