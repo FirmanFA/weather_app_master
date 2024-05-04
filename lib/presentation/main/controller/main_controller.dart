a15f921b360480704d31149d416e0f8044fc4554
firmanfa <fafirmansyah20@gmail.com>
2024-05-04 09:03:38 +0700

    feat<main_controller>: create get weather method

--- lib/presentation/main/controller/main_controller.dart
+++ lib/presentation/main/controller/main_controller.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:get/get.dart';
+import 'package:weather_app_master/core/remote/api_result.dart';
+import 'package:weather_app_master/core/repository/weather_repository.dart';
+import 'package:weather_app_master/presentation/main/model/weather/get_weather_response.dart';
 import 'package:weather_app_master/utils/location_util.dart';
 
 class MainController extends GetxController {
   var selectedCityString = "".obs;
   Rxn<Location> selectedLocation = Rxn();
+
+  final _weatherRepository = WeatherRepository();
+
+  Rx<UIState<GetWeatherResponse>> currentWeatherState = Rx(UiLoading());
 
   @override
   void onInit() {
@@ -19,7 +26,21 @@ class MainController extends GetxController {
       if (value != null) {
         setSelectedCity(await value.getCityName);
         setSelectedLocation(value.toLocation);
+        getCurrentWeather();
       } else {}
+    });
+  }
+
+  getCurrentWeather() {
+    currentWeatherState.value = UiLoading();
+    _weatherRepository.getCurrentWeather(queryParams: {
+      'lon': selectedLocation.value?.longitude,
+      'lat': selectedLocation.value?.latitude,
+    }).then((value) {
+      currentWeatherState.value = UiSuccess(value);
+    }).catchError((er) {
+      Get.snackbar("Gagal", er.message);
+      currentWeatherState.value = UiFailure(er.message);
     });
   }
 
